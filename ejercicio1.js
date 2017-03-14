24ecf7ba2391b66642996b9ae4943b7994f7c512
Cecilia Cerqueda <pzcerqueda@gmail.com>
2017-03-14 08:50:43 -0600

    ejer-stack-queue

---
+++ ejercicio1.js
@@ -0,0 +1,12 @@
+var nombres = [];
+
+
+var nombre= newStack(0,1);
+function Stack(){
+nombres.push ("luna");
+nombres.push("totoro");
+nombres.push ("ponyo");
+nombres.push ("sabrina");
+nombres.push ("goku");
+}
+console.log(Stack.getLength()); // 3
